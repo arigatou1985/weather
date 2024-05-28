acb681c09cbe8848f5ef3fc6c99cfde54cffa951
Jing Yu <arigatou1985@gmail.com>
2024-05-28 11:46:30 +0200

    Add Xcode project

---
+++ Weather/Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Jing Yu on 2024-05-28.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
